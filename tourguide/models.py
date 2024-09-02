12cef62900898a1bf6da19a3691bd8231a6e7724
Isabela9717 <isabelachana@gmail.com>
2024-09-02 15:09:30 +0200

    converted to django

---
+++ tourguide/models.py
@@ -0,0 +1,101 @@
+from django.db import models
+import datetime
+from django.utils.translation import gettext_lazy as _
+
+
+# Province model
+class Province(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    capitalCity = models.TextField(max_length=100)
+    description = models.TextField(max_length=350, default='', blank=True, null=True)
+    image = models.ImageField(upload_to='provinces/')
+    
+    def __str__(self):
+        return self.name
+
+# Tourism sites model
+class TourismSite(models.Model):
+    name = models.CharField(max_length=200)
+    province = models.ForeignKey(Province, on_delete=models.CASCADE)
+    site_type = models.CharField(max_length=100)
+    description = models.TextField(max_length=350, default='', blank=True, null=True)
+    image = models.ImageField(upload_to='tourism_sites/')
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    booking_link = models.URLField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+# Hotel model
+class Hotel(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=255)
+    province = models.ForeignKey(Province, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='hotels/')
+    description = models.TextField(max_length=350, default='', blank=True, null=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    booking_link = models.URLField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+# Rental car model
+class RentalCar(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=255)
+    province = models.ForeignKey(Province, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='hotels/')
+    description = models.TextField(max_length=350, default='', blank=True, null=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    booking_link = models.URLField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+# Customer or user model
+class User(models.Model):
+    firstName = models.CharField(max_length=200)
+    lastName = models.CharField(max_length=255)
+    phone = models.ForeignKey(Province, on_delete=models.CASCADE)
+    email = models.ImageField(upload_to='hotels/')
+    password = models.TextField()
+    
+    def __str__(self):
+        return self.name
+
+# Reviews model
+class Review(models.Model):
+    RATING_CHOICES = [
+        (1, _('1 - Poor')),
+        (2, _('2 - Fair')),
+        (3, _('3 - Good')),
+        (4, _('4 - Very Good')),
+        (5, _('5 - Excellent')),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tourism_site = models.ForeignKey('TourismSite', on_delete=models.CASCADE, blank=True, null=True)
+    hotel = models.ForeignKey('Hotel', on_delete=models.CASCADE, blank=True, null=True)
+    rental_car = models.ForeignKey('RentalCar', on_delete=models.CASCADE, blank=True, null=True)
+    rating = models.PositiveSmallIntegerField(choices=RATING_CHOICES)
+    text = models.TextField()
+
+    def __str__(self):
+        return f"{self.user.username} - {self.rating} stars"
+
+# rating model
+class Rating(models.Model):
+    entity_type = models.CharField(max_length=20, choices=[
+        ('tourism_site', _('Tourism Site')),
+        ('hotel', _('Hotel')),
+        ('rental_car', _('Rental Car')),
+    ])
+    entity_id = models.PositiveIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(choices=Review.RATING_CHOICES)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.get_entity_type_display()} {self.entity_id} - {self.rating} stars"
